a49691c049cef7dc3aa1591dd00a67bc684c498a
adamblum <adam@openedinstitute.org>
2013-11-16 11:39:36 -0800

    add contribution name

--- opened_sample.rb
+++ opened_sample.rb
@@ -3,10 +3,11 @@ require 'json'
 
 phrase="Fibonacci%20sequence"
 limit=10
-response=RestClient.get 'https://api.opened.io/resources.json?descriptive=#{phrase}&limit=#{limit}'
+response=RestClient.get "https://api.opened.io/resources.json?descriptive=#{phrase}&limit=#{limit}"
 result=JSON.parse(result)
 
-p "Resources return\n=============="
+p "Resources returned"
+p "=================="
 resources=result["resources"]
 resources.each do |r|
   p "Resource: #{r['title']}: #{r['share_url']}"
