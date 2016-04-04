47104103f80b32faf17efcb1de4622a3d650f00e
lucascampbell <lkecamp1@gmail.com>
2016-04-04 14:39:34 -0700

    added dread support

---
+++ api_hooks.rb
@@ -0,0 +1,132 @@
+require 'json'
+include DreddHooks::Methods
+
+stash     = {}
+stash['static_group_id'] = '31775'
+CLIENT_ID = ENV['CLIENT_ID']
+SECRET    = ENV['SECRET']
+
+before 'Accounts > Access Token > Getting your Access Token' do |transaction|
+  request_body = JSON.parse transaction['request']['body']
+  request_body['client_id'] = CLIENT_ID
+  request_body['secret']    = SECRET
+  request_body['username']  = 'testuser' 
+  transaction['request']['body'] = request_body.to_json
+end
+
+# hook to retrieve session on a login
+after 'Accounts > Access Token > Getting your Access Token' do |transaction|
+  parsed_body = JSON.parse transaction['real']['body']
+  stash['token'] = parsed_body['access_token']
+end
+
+# hook to set the session cookie in all following requests
+before_each do |transaction|
+  unless stash['token'].nil?
+    transaction['request']['headers']['Authorization'] = "Bearer " + stash['token']
+  end
+end
+
+after "Manage teacher's classes and students > Teacher's Classes > Create a New Class" do |transaction|
+  parsed_body = JSON.parse transaction['real']['body']
+  stash['group_id'] = parsed_body['class']['id'].to_s
+end
+
+after "Manage teacher's classes and students > Teacher's Classes > Create a Student" do |transaction|
+  parsed_body = JSON.parse transaction['real']['body']
+  stash['student_id'] = parsed_body['student']['id'].to_s
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Create a Student" do |transaction|
+  request_body = JSON.parse transaction['request']['body']
+  request_body['student']['username'] = 'studentapi'
+  request_body['student']['class_ids'] = []
+  transaction['request']['body'] = request_body.to_json
+end
+
+before "Manage teacher's classes and students > Teacher's Students > Update Student" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['student_id']
+end
+
+before "Resources > Search Resources > Search" do |transaction|
+  path = transaction['fullPath'].split("?")[0]
+  transaction['fullPath'] =  "#{path}?license=free"
+end
+
+before "Assessment Results > Get Assessment Results > GET" do |transaction|
+  # request_body = JSON.parse transaction['request']['body']
+  # request_body = {}
+  # request_body['resource_id'] = '1069949'
+  # transaction['request']['body'] = request_body.to_json
+  puts "full path is #{transaction['fullPath']}"
+end
+
+after "Assessment Results > Get Assessment Results > GET" do |transaction|
+  parsed_body = JSON.parse transaction['real']['body']
+end
+
+before "Manage teacher's classes and students > Teacher's Students > Remove a Student" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['student_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Get Class" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['group_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Update Class" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['group_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Remove a Class" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['group_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Students > Get Student" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['student_id']
+end
+
+before "User > Update a User > Update" do |transaction|
+  request_body = JSON.parse transaction['request']['body']
+  request_body['school_nces_id']= 360008702155
+  request_body['first_name'] = "Update User first name"
+  request_body['last_name'] = "Update User last name"
+  transaction['request']['body'] = request_body.to_json
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Add existing students to the Class" do |transaction|
+  request_body = JSON.parse transaction['request']['body']
+  request_body['student_ids'] = [stash['student_id']]
+  transaction['request']['body'] = request_body.to_json
+  transaction['fullPath'].gsub! '123', stash['static_group_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Remove students from the Class" do |transaction|
+  request_body = JSON.parse transaction['request']['body']
+  request_body['student_ids'] = [stash['student_id']]
+  transaction['request']['body'] = request_body.to_json
+  transaction['fullPath'].gsub! '123', stash['static_group_id']
+end
+
+before "Manage teacher's classes and students > Teacher's Classes > Remove a Class" do |transaction|
+  transaction['fullPath'].gsub! '123', stash['group_id']
+end
+
+# skip create tests
+
+before "User > Create a User > Create" do |transaction|
+  transaction['skip'] = true
+end
+
+before "Resources > Create Resource > Create" do |transaction|
+  transaction['skip'] = true
+end
+
+before "Resources > Update Resource > Update" do |transaction|
+  transaction['skip'] = true
+end
+
+before "School > Create a School > Create" do |transaction|
+  transaction['skip'] = true
+end
+
+
